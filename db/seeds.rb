e336f9800332c283355e9430fbdfb1baa7ab6014
minheekangg <minheekangg@gmail.com>
2018-10-26 11:32:35 -0400

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,46 @@
 #PARKS+REC - RON SWANSON API
-response_string = RestClient.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes/30')
+response_string = RestClient.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes/50')
 response_hash = JSON.parse(response_string)
   response_hash.each do |each_quote|
     Game.create(show: "Parks and Rec", quote: each_quote)
   end
 
 #THE OFFICE
-30.times do
+50.times do
   Game.find_or_create_by(show: "The Office", quote: Faker::MichaelScott.quote)
 end
 
 #SEINFELD
-30.times do
+50.times do
   Game.find_or_create_by(show: "Seinfeld", quote: Faker::Seinfeld.quote)
 end
 
 #FRIENDS
-30.times do
+50.times do
   Game.find_or_create_by(show: "Friends", quote: Faker::Friends.quote)
 end
 
-#FRIENDS
-30.times do
-  Game.find_or_create_by(show: "New Girl", quote: Faker::NewGirl.quote)
+# #NEW GIRL
+# 50.times do
+#   Game.find_or_create_by(show: "New Girl", quote: Faker::NewGirl.quote)
+# end
+
+#IT CROWD
+50.times do
+  Game.find_or_create_by(show: "The IT Crowd", quote: Faker::TheITCrowd.quote)
+end
+
+# #Silicon Valley
+# 50.times do
+#   Game.find_or_create_by(show: "Silicon Valley", quote: Faker::SiliconValley.quote)
+# end
+
+#How I Met Your Mother
+50.times do
+  Game.find_or_create_by(show: "How I Met Your Mother", quote: Faker::HowIMetYourMother.quote)
+end
+
+
+20.times do
+  Score.create(user_id: 1, game_id: 3, score: 0, username: "mh")
 end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -6,12 +6,30 @@ class CLI
 
  def initialize
    @prompt = TTY::Prompt.new
+   @artii = Artii::Base.new  #:font => 'slant'
+   @cursor = TTY::Cursor
  end
 
  def welcome
+  puts @artii.asciify('SITCOM TRIVIA!').colorize(:color => :white, :background => :blue)
+  puts "              |  /                                                                         ".colorize(:color => :white, :background => :blue)
+  puts "              |/                                                                           ".colorize(:color => :white, :background => :blue)
+  puts "         .===============.                                                                 ".colorize(:color => :white, :background => :blue)
+  puts "         | .-----------. |                                                                 ".colorize(:color => :white, :background => :blue)
+  puts "         | |  SITCOM   | |                                                                 ".colorize(:color => :white, :background => :blue)
+  puts "         | |  TRIVIA   | |                                                                 ".colorize(:color => :white, :background => :blue)
+  puts "         | |  !!!!!!!  | |   __                                                            ".colorize(:color => :white, :background => :blue)
+  puts "         | '-----------'o|  |o.|                                                           ".colorize(:color => :white, :background => :blue)
+  puts "         |===============|  |::|                                                           ".colorize(:color => :white, :background => :blue)
+  puts "         |###############|  |::|                                                           ".colorize(:color => :white, :background => :blue)
+  puts "         '==============='  '--'                                                           ".colorize(:color => :white, :background => :blue)
+
+  sleep(1)
+  puts "\n"
    puts "Welcome! Please enter your name and hit ENTER."
    username = gets.chomp   #TAKES IN USERNAME
-   puts "Hi #{username}, let's play sitcom trivia!!!"
+   puts "Hi #{username}, let's play sitcom trivia!!!".colorize(:color => :cyan)
+   sleep(1)
    puts "\n"
    new_user = User.find_or_create_by(username: username)
    @user = new_user
@@ -23,8 +41,14 @@ class CLI
    get_game =[]
    get_game << Game.all
    @curr_game = get_game.flatten
-
-   puts "Try to guess which show the following quote belongs to."
+   puts "\n"
+   puts "Try to guess which show the following quote belongs to.".colorize(:color => :magenta)
+   puts "3"
+   sleep(1)
+   puts "2"
+   sleep(1)
+   puts "1"
+   sleep(1)
    puts "\n"
    game_instance = @curr_game.sample
    quote = game_instance.quote
@@ -41,6 +65,7 @@ class CLI
     quote = game_instance.quote        #takes quote from picked instance
 
     show_names = Game.all.map { |game| game.show }.uniq     #collects show names
+    puts "NEXT QUOTE:".colorize(:color => :magenta)
     answer = @prompt.select(quote, show_names)              #SHOWS Q+A
 
     @curr_game.delete(game_instance)
@@ -51,7 +76,7 @@ class CLI
  def check_answer(quote, answer, game_instance)
    show = Game.all.find_by(quote: quote).show
    if (show == answer)
-     puts "CORRECT!"
+     puts "CORRECT!".colorize(:color => :green)
      @game_score += 1
      @shows << show
      puts "Your score is #{self.game_score}."
@@ -59,7 +84,7 @@ class CLI
      continue_curr_game
 
    else
-     puts "UHOH! The correct answer is: #{show}"
+     puts "UHOH! The correct answer is: #{show}".colorize(:color => :white, :background => :red)
      puts "\n"
      @shows << show
      @shows.pop
@@ -67,13 +92,13 @@ class CLI
      save_score
      display_score(self.game_score, @shows)
      puts "\n"
-     binding.pry
      play_again
    end
  end
 
  def display_score(score, shows)
    puts "\tYour score for this game is #{score}"
+   puts "\t------------------------------"
    show_count = Hash.new(0)
    shows.each do |show|
      show_count[show] +=1
@@ -96,18 +121,47 @@ class CLI
      start_game
    elsif answer == "no"
      display_high_score
+     display_hardest_question
    else
      play_again
    end
  end
 
  def display_high_score
-   puts "HIGH SCORES"
+   sleep(1)
+   # puts @artii.asciify('HIGH SCORES')
    sorted_score =  Score.all.sort_by {|obj| obj.score}.reverse
    top10 = sorted_score[0..9]
+   # binding.pry
+   rows = []
    top10.each do |each|
-     puts "#{each.username} : #{each.score}"
+     rows << [each.username, each.score]
    end
+   table = Terminal::Table.new :title => "HIGH SCORES", :headings => ['Username', 'Score'], :rows => rows, :style => {:width => 40, :padding_left => 3, :border_x => "=", :border_i => "x"}
+   puts table
  end
+
+ def display_hardest_question
+   if Score.all.count <= 20
+     puts "Play more to get the hardest question!"
+   else
+       q_arr = Score.all.map do |each_score|
+         each_score.game_id
+       end
+       q_count = Hash.new(0)
+       q_arr.each do |q|
+         q_count[q] +=1
+       end
+       sorted_q = q_count.sort_by {|k,v| v}.reverse
+       q_id = sorted_q[0][0]
+
+       q_instance = Game.find_by id: q_id
+       sleep(1)
+       puts "\n"
+       puts "Hardest Question: #{q_instance.quote}".colorize(:color => :cyan)
+       puts "\n"
+     end
+ end
+
 
 end # end of cli method
